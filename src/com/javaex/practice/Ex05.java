59faa4b3efc682a8562b6cb11517dfec38ad1c77
현 <usesr@DESKTOP-3RO9PAV>
2021-12-15 01:48:50 +0900

    .

---
+++ src/com/javaex/practice/Ex05.java
@@ -0,0 +1,28 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int[] dataArray = new int[5];
+		int num;
+		double sum = 0;
+
+		for (int i = 0; i < dataArray.length; i++) {
+			num = sc.nextInt();
+			dataArray[i] = num;
+		}
+
+		for (int i = 0; i < dataArray.length; i++) {
+			sum = sum + dataArray[i];
+		}
+
+		System.out.println("평균은 " + sum / dataArray.length + " 입니다.");
+
+		sc.close();
+	}
+
+}
